21bbf275eab6d436ff69dec037879945a10d9a24
Ragibnoor <ragibnoor8766@gmail.com>
2026-07-27 22:57:36 +0530

    New and Create Route

--- app.js
+++ app.js
@@ -32,12 +32,24 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", {allListing});
 });
 
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 // Show Route
 app.get("/listings/:id", async (req, res) => {
   let {id} = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", {listing});
 })
+
+// Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
